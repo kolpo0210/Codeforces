4df87314dfe80d5e9455488f64a85da73cf16e32
MD SAMS ARAFIN KOLPO <88968366+kolpo0210@users.noreply.github.com>
2025-04-14 12:08:23 +0600

    Create A_Trippi_Troppi.cpp

---
+++ Codeforces Round 1017 (Div. 4)/A_Trippi_Troppi.cpp
@@ -0,0 +1,19 @@
+//https://codeforces.com/contest/2094/submission/315325624
+//https://codeforces.com/profile/Sams_Arafin_Kolpo
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        string s1;
+        string s2;
+        string s3;
+
+        cin >> s1 >> s2 >> s3;
+        cout << s1[0] << s2[0] << s3[0] << endl;
+    }
+    return 0;
+}
